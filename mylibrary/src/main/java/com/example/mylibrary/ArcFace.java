6dfe5f20fb830f1b30b79bf226bddaef64e7de2e
lee <666888@qq.com>
2023-09-05 22:06:11 +0800

    修改ArcFace类

---
+++ mylibrary/src/main/java/com/example/mylibrary/ArcFace.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import com.arcsoft.face.ErrorInfo;
+import com.arcsoft.face.FaceEngine;
+import com.arcsoft.face.model.ActiveDeviceInfo;
+
+public class ArcFace extends FaceEngine {
+
+
+}
